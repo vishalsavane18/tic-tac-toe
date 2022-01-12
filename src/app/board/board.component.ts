6da18e590785756e9424ade12c7011f55c42187b
Vishal's lenovo <vishalsavane18@gmail.com>
2022-01-12 21:13:20 +0530

    first commit

---
+++ src/app/board/board.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-board',
+  templateUrl: './board.component.html',
+  styleUrls: ['./board.component.css'],
+})
+export class BoardComponent implements OnInit {
+  squares: any[];
+  xIsNext: boolean;
+  winner: string;
+
+  constructor() {}
+
+  ngOnInit() {
+    this.newGame();
+  }
+  newGame() {
+    this.squares = Array(9).fill(null);
+    this.winner = null;
+    this.xIsNext = true;
+  }
+  getPlayer(){
+    return this.xIsNext ? 'X' | 'O';
+  }
+}
---
+++ src/app/square/square.component.ts
@@ -0,0 +1,10 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-square',
+  template: `<button>{{ Value }}</button> `,
+  styles: [],
+})
+export class SquareComponent {
+  @Input() Value: 'X' | 'O' | undefined;
+}
